af9959538a8300cc792e31a0f8e8c37592194099
Mvykel <michal.lukasz111@gmail.com>
2025-03-16 16:02:05 +0100

    first commit

--- src/wsb/po/jeden/Pierwsza.java
+++ src/wsb/po/jeden/Pierwsza.java
@@ -2,7 +2,6 @@ package wsb.po.jeden;
 
 public class Pierwsza {
     public static void main(String[] args) {
-        System.out.println("Hello.");
-        System.out.println("Hello.");
+        System.out.println("First change.");
     }
 }
